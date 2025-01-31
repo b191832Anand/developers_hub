1573af85c4058a3db742961711c0b3fee210634b
anand <anandkumarch2003@gmail.com>
2025-01-31 13:00:40 +0530

    "e"

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures assets load correctly on Vercel
+  base: '/', 
   build: {
     outDir: 'dist',
   },
